b3520797fab50240943151936f2b0454f71c5970
Serhii Semeniaka <Serh.semka@gmail.com>
2026-04-15 16:36:58 +0300

    Set up database migration, Docker support, and compose configuration for initial project deployment

--- PersonalBlog.API/Program.cs
+++ PersonalBlog.API/Program.cs
@@ -1,5 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using PersonalBlog.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<BlogDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<BlogDbContext>();
+        
+        await context.Database.MigrateAsync();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
 
 app.MapGet("/", () => "Hello World!");
 
