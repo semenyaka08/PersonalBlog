104986e3708a4dc91cb14ddf3ace78a49996822a
Serhii Semeniaka <Serh.semka@gmail.com>
2026-04-18 13:56:55 +0300

    Integrate IdentityDbContext in `BlogDbContext` to support Identity framework with `ApplicationUser` and `ApplicationRole`.

--- PersonalBlog.DAL/BlogDbContext.cs
+++ PersonalBlog.DAL/BlogDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using PersonalBlog.DAL.Entities;
 
 namespace PersonalBlog.DAL;
 
-public class BlogDbContext : DbContext
+public class BlogDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
 {
     public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
     {
